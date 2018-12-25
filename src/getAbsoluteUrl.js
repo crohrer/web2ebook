0286d9d75984f2db0e3d9343de65d5e5e0339cb7
Christoph Rohrer <christoph.rohrer@odt.net>
2018-12-25 22:46:33 +0100

    fixes url helper for urls with -

--- src/getAbsoluteUrl.js
+++ src/getAbsoluteUrl.js
@@ -4,7 +4,7 @@ function getAbsoluteUrl({urlWithDomain, relativeUrl}){
         const domain = urlWithDomain.match(/(https?:\/\/[a-z\.A-Z~0-9]+)\//)[1];
         return domain + relativeUrl
     }
-    const path = urlWithDomain.match(/(https?:\/\/[a-z\.A-Z~0-9_\/]+\/)([a-z\.A-Z~0-9_])+$/)[1];
+    const path = urlWithDomain.match(/(https?:\/\/[a-z\.A-Z~0-9_\/-]+\/)([a-z\.A-Z~0-9_-])+$/)[1];
     return path + relativeUrl
 }
 
